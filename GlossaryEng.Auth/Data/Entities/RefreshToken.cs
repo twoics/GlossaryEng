e62bedc487c51a96d888955d3760ddf5e1242a4d
twoics-linux <twoics@mail.ru>
2022-08-26 23:47:56 +0700

    Add constructor to RefreshToken entity

--- GlossaryEng.Auth/Data/Entities/RefreshToken.cs
+++ GlossaryEng.Auth/Data/Entities/RefreshToken.cs
@@ -2,6 +2,12 @@ namespace GlossaryEng.Auth.Data.Entities;
 
 public class RefreshToken
 {
+    public RefreshToken(string token, string userDbId)
+    {
+        Token = token;
+        UserDbId = userDbId;
+    }
+    
     public int Id { get; set; }
     public string Token { get; set; }
     public string UserDbId { get; set; }
